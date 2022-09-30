8cde15fd9070295fc92ba14c58b676ad75aa23d3
amitaudifytech <amit@audifytech.com>
2022-09-30 15:39:10 +0200

    Initial Commit

---
+++ MVVMWithAlmofire/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMWithAlmofire
+//
+//  Created by Amit on 30/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
